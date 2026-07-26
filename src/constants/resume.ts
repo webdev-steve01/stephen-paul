9560f30f117e149386d718c20a6afff5d60e3b1f
webdev-steve01 <sylvesterpaulstephen1234@gmail.com>
2026-07-26 19:00:03 +0100

    change: changed from js to fe developer

--- src/constants/resume.ts
+++ src/constants/resume.ts
@@ -1,1 +1,6 @@
-export const resume_download = "https://drive.google.com/uc?export=download&id=1jpaLb4EnBkmnG2dMg1sMyHS16zqrC93O";
+
+// javascript developer
+// export const resume_download = "https://drive.google.com/uc?export=download&id=1jpaLb4EnBkmnG2dMg1sMyHS16zqrC93O";
+
+// frontend developer
+export const resume_download = "https://drive.google.com/uc?export=download&id=1X7EFZuL_-C26BY55su57nTbop-fAdaW"
