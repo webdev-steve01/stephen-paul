31bbe6b9b5424f6936a1442797bbd02fcf48073c
webdev-steve01 <sylvesterpaulstephen1234@gmail.com>
2026-07-26 19:08:22 +0100

    fix: added headless cms to resume

--- src/constants/resume.ts
+++ src/constants/resume.ts
@@ -3,5 +3,6 @@
 // export const resume_download = "https://drive.google.com/uc?export=download&id=1jpaLb4EnBkmnG2dMg1sMyHS16zqrC93O";
 
 // frontend developer
-export const resume_download = "https://drive.google.com/uc?export=download&id=1X7EFZuL_-C26BY55su57nTbop-fAdaWK"
+export const resume_download = "https://drive.google.com/uc?export=download&id=1KHnm1-2fLtJP2T87yIQYnrzfiYYVJdd_"
+// https://drive.google.com/file/d//view?usp=sharing
 // https://drive.google.com/file/d/1X7EFZuL_-C26BY55su57nTbop-fAdaWK/view?usp=sharing
