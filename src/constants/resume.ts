4076ef0727853f253e1690e30c3726e6265f69aa
webdev-steve01 <sylvesterpaulstephen1234@gmail.com>
2026-07-26 19:01:20 +0100

    fix: fixed broken url

--- src/constants/resume.ts
+++ src/constants/resume.ts
@@ -3,4 +3,5 @@
 // export const resume_download = "https://drive.google.com/uc?export=download&id=1jpaLb4EnBkmnG2dMg1sMyHS16zqrC93O";
 
 // frontend developer
-export const resume_download = "https://drive.google.com/uc?export=download&id=1X7EFZuL_-C26BY55su57nTbop-fAdaW"
+export const resume_download = "https://drive.google.com/uc?export=download&id=1X7EFZuL_-C26BY55su57nTbop-fAdaWK"
+// https://drive.google.com/file/d/1X7EFZuL_-C26BY55su57nTbop-fAdaWK/view?usp=sharing
